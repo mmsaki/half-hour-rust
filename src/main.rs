7e25bf9c28a71d968b0fe0a585f76148d9cd506e
Meek <meek10x@gmail.com>
2023-03-31 06:11:34 -0500

    implementing  Neg to our type

--- src/main.rs
+++ src/main.rs
@@ -376,4 +376,29 @@ fn main() {
         main()
     }
 
+    // A foreign trait on our type:
+    // the 'Neg' trait is used to overload '-', the unary minus operator
+    {
+        impl std::ops::Neg for Number {
+            type Output = Number;
+
+            fn neg(self) -> Number {
+                Number {
+                    value: -self.value,
+                    odd: self.odd,
+                }
+            }
+        }
+
+        fn main() {
+            let n = Number {
+                odd: true,
+                value: 987,
+            };
+            let m = -n; // this is only posible because we implemented 'Neg'
+            println!("{}", m.value); // prints '-987'
+        }
+        main()
+    }
+
 }
